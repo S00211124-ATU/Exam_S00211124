2b18138ce6751b76bba8ce3517eeffb7bd9d5053
s00211124 <s00211124@LAB-D2036-23>
2022-05-17 09:39:43 +0100

    added properties

---
+++ RentalProperty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_S00211124
+{
+    class RentalProperty
+    {
+        // properties
+        public int ID { get; set; }
+        public enum RentalType
+        {
+            House,
+            Flat,
+            Share
+        }
+        public string Location { get; set; }
+
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
